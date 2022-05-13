f81b45e16baeb15c3b01774867d7105dfa3d3b8d
Khalid Alabi <81325840+DeanSft-dev@users.noreply.github.com>
2022-05-13 02:48:11 +0100

    done with navbar and home page

---
+++ js/app.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    //HERO SLIDER
+    $('#hero-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        items:1,
+        dots:false,
+        smartSpeed:1000,
+        navText:['PREV', 'NEXT'],
+        responsive:{
+            0:{
+            
+            },
+            600:{
+                
+            },
+            1000:{
+                
+            }
+        }
+    })
+});
